23743c1837bc593f081d5f58fafa695658322b6c
mchao409 <mchao409@gmail.com>
2018-01-25 01:40:34 -0500

    Test code in 'API testing' jupyter notebook

--- example_plugin.py
+++ example_plugin.py
@@ -5,7 +5,12 @@ import sublime_plugin
 
 class ExampleCommand(sublime_plugin.TextCommand):
 	def run(self, edit):
-		self.view.insert(edit, 0, "Hello, World!")
+		import urllib
+		import webbrowser
+		# r = urllib.request.urlopen(url)
+		webbrowser.open(url,new=0,autoraise=True)
+
+		# self.view.insert(edit, 0, "Hello, World!")
 
 
 
