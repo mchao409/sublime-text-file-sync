c15c6f24105640ec2d6b0b629634afeb5cc33f2f
mchao409 <mchao409@gmail.com>
2018-01-25 23:16:47 -0500

    add server/client code for dropbox api

--- example_plugin.py
+++ example_plugin.py
@@ -5,10 +5,20 @@ import sublime_plugin
 
 class ExampleCommand(sublime_plugin.TextCommand):
 	def run(self, edit):
-		import urllib
 		import webbrowser
+		import http.server
+		import socketserver
+		from urllib import request
+		import urllib
+		import http.server
+		import socket
+		import sys
+		import re
+		import time
+
+		token = "P5J33P1PKRAAAAAAAAAAH0ETP_ZEVQAWPKSDE69PY44"
 		# r = urllib.request.urlopen(url)
-		webbrowser.open(url,new=0,autoraise=True)
+		# webbrowser.open(url,new=0,autoraise=True)
 
 		# self.view.insert(edit, 0, "Hello, World!")
 
