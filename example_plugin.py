1dd67b94d3130796080c1b03b8854acb9a3e4fb6
mchao409 <mchao409@gmail.com>
2018-01-28 16:48:39 -0500

    update the server code, start dropbox class

--- example_plugin.py
+++ example_plugin.py
@@ -7,20 +7,14 @@ class ExampleCommand(sublime_plugin.TextCommand):
 	def run(self, edit):
 		import webbrowser
 		import http.server
-		import socketserver
 		from urllib import request
 		import urllib
-		import http.server
-		import socket
-		import sys
-		import re
-		import time
+		import json
 
-		token = "P5J33P1PKRAAAAAAAAAAH0ETP_ZEVQAWPKSDE69PY44"
 		# r = urllib.request.urlopen(url)
 		# webbrowser.open(url,new=0,autoraise=True)
 
-		# self.view.insert(edit, 0, "Hello, World!")
+		self.view.insert(edit, 0, str(files))
 
 
 
